16c9a2a42bb9c26060a949ca093dedbcf45d1ab3
Evan Boehs <evan@boehs.org>
2022-10-16 21:22:04 -0400

    add typings

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 /// Function that generates rgba-strings
-export const R = (r,g,b,a)=>(a =(a === undefined ? 1 : a),"rgba("+(r|0)+","+(g|0)+","+(b|0)+","+a+")")
+export const R = (r: number,g: number,b: number,a?: number)=>(a =(a === undefined ? 1 : a),"rgba("+(r|0)+","+(g|0)+","+(b|0)+","+a+")")
 
 /// Shorthand for Math.sin
-export const S = n => Math.sin(n)
+export const S = (n: number) => Math.sin(n)
 /// Shorthand for Math.cos
-export const C = n => Math.cos(n)
+export const C = (n: number) => Math.cos(n)
 /// Shorthand for Math.tan
-export const T = n => Math.tan(n)
+export const T = (n: number) => Math.tan(n)
 
 /// Shorthand for Math.PI
 export const PI = Math.PI
